08d6b998cf40cedf76637c6fda2228a690da907e
DavidYo <lturtsamuel@gmail.com>
2019-09-06 22:52:50 +0800

    讓 babel 自動編譯 typescript

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 搭建開發環境請參閱以下連結
 https://reactnative.cn/docs/getting-started/
+
+如果 `yarn run-android` 無法順利開啟 JS Server，可以在一個終端裡執行 `yarn start`，另一個終端執行 `yarn run-android`。
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-	presets: ['module:metro-react-native-babel-preset'],
+	'presets': [
+		'module:metro-react-native-babel-preset',
+	],
+	'plugins': [
+		[
+			'module-resolver',
+			{
+				'root': ['./src'],
+				'extensions': ['.js', '.ts', '.tsx', '.ios.js', '.android.js']
+			}
+		],
+		'@babel/plugin-transform-runtime',
+	]
 };
