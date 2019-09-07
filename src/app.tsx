d05913affde344318f02ca3ac03debebf95362e7
DavidYo <lturtsamuel@gmail.com>
2019-09-07 21:04:20 +0800

    用 react-navigation 定出導覽流程

--- src/app.tsx
+++ src/app.tsx
@@ -1,119 +1,96 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React from 'react';
+import { View, Text, Button } from 'react-native';
 
-import React, {Fragment} from 'react';
-import {
-	SafeAreaView,
-	StyleSheet,
-	ScrollView,
-	View,
-	Text,
-	StatusBar,
-} from 'react-native';
+import { createAppContainer, NavigationScreenProps } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+// @ts-ignore
+import { createBottomTabNavigator, createMaterialTopTabNavigator } from 'react-navigation-tabs';
 
-import {
-	Header,
-	LearnMoreLinks,
-	Colors,
-	DebugInstructions,
-	ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const _usingHermes = typeof HermesInternal === 'object' && HermesInternal !== null;
 
-const styles = StyleSheet.create({
-	scrollView: {
-		backgroundColor: Colors.lighter,
-	},
-	engine: {
-		position: 'absolute',
-		right: 0,
-	},
-	body: {
-		backgroundColor: Colors.white,
-	},
-	sectionContainer: {
-		marginTop: 32,
-		paddingHorizontal: 24,
-	},
-	sectionTitle: {
-		fontSize: 24,
-		fontWeight: '600',
-		color: Colors.black,
-	},
-	sectionDescription: {
-		marginTop: 8,
-		fontSize: 18,
-		fontWeight: '400',
-		color: Colors.dark,
-	},
-	highlight: {
-		fontWeight: '700',
-	},
-	footer: {
-		color: Colors.dark,
-		fontSize: 12,
-		fontWeight: '600',
-		padding: 4,
-		paddingRight: 12,
-		textAlign: 'right',
-	},
-});
-
-
-function App(): JSX.Element {
-	const usingHermes = typeof HermesInternal === 'object' && HermesInternal !== null;
-
+function HotContent(props: NavigationScreenProps): JSX.Element {
 	return (
-		<Fragment>
-			<StatusBar barStyle="dark-content" />
-			<SafeAreaView>
-				<ScrollView
-					contentInsetAdjustmentBehavior="automatic"
-					style={styles.scrollView}>
-					<Header />
-					{!usingHermes ? null : (
-						<View style={styles.engine}>
-							<Text style={styles.footer}>Engine: Hermes</Text>
-						</View>
-					)}
-					<View style={styles.body}>
-						<View style={styles.sectionContainer}>
-							<Text style={styles.sectionTitle}>Step One</Text>
-							<Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                screen and then come back to see your edits.
-							</Text>
-						</View>
-						<View style={styles.sectionContainer}>
-							<Text style={styles.sectionTitle}>See Your Changes</Text>
-							<Text style={styles.sectionDescription}>
-								<ReloadInstructions />
-							</Text>
-						</View>
-						<View style={styles.sectionContainer}>
-							<Text style={styles.sectionTitle}>Debug</Text>
-							<Text style={styles.sectionDescription}>
-								<DebugInstructions />
-							</Text>
-						</View>
-						<View style={styles.sectionContainer}>
-							<Text style={styles.sectionTitle}>Learn More</Text>
-							<Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-							</Text>
-						</View>
-						<LearnMoreLinks />
-					</View>
-				</ScrollView>
-			</SafeAreaView>
-		</Fragment>
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>碳鍵熱門內容！</Text>
+			<Button
+				title="Go to Article"
+				onPress={() => props.navigation.navigate('Article')}
+			/>
+		</View>
 	);
 };
 
-export default App;
+function Board(props: NavigationScreenProps): JSX.Element {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>某個看板，應該包含許多文章</Text>
+			<Button
+				title="Go to Article"
+				onPress={() => props.navigation.navigate('Article')}
+			/>
+		</View>
+	);
+}
+
+
+
+function BoardList(props: NavigationScreenProps): JSX.Element {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>這頁應該包含我的最愛和熱目看板！</Text>
+			<Button
+				title="Go to Board"
+				onPress={() => props.navigation.navigate('Board')}
+			/>
+		</View>
+	);
+}
+
+const ForumContent = createStackNavigator({
+	HotContent: HotContent,
+	Board: Board,
+}, {
+	headerMode: 'none',
+	initialRouteName: 'HotContent'
+});
+
+const ForumBody = createMaterialTopTabNavigator({
+	ForumContent: ForumContent,
+	BoardList: BoardList
+}, {
+	initialRouteName: 'ForumContent',
+});
+
+function Message(): JSX.Element {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>通知！</Text>
+		</View>
+	);
+}
+
+const TabNavigator = createBottomTabNavigator({
+	ForumBody: ForumBody,
+	Message: Message
+}, {
+	initialRouteName: 'ForumBody',
+});
+
+function Article(): JSX.Element {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>測試文章！</Text>
+		</View>
+	);
+}
+
+const AppNavigator = createStackNavigator({
+	TabNavigator: TabNavigator,
+	Article: Article
+}, {
+	headerMode: 'none',
+	mode: 'modal',
+	initialRouteName: 'TabNavigator'
+});
+
+export default createAppContainer(AppNavigator);
